8b2f7258efcac7fc1856984a0ff8c98554ddce46
YuHai~ <1501853627@qq.com~>
2026-04-18 21:06:00 +0800

    style: 调整 v2rayA 摘要卡为整排铺开

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            appVersion: 'v10.1.12',
+            appVersion: 'v10.1.13',
             versionPageUrl: 'https://github.com/YuHaiA/opaiRe/releases/latest',
             isLoggedIn: !!localStorage.getItem('auth_token'),
             loginPassword: '',
