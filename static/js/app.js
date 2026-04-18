caf2cf4b3b52ecc19f7c7bf6f335a6e101348c31
YuHai~ <1501853627@qq.com~>
2026-04-18 22:47:36 +0800

    release: v10.1.17

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            appVersion: 'v10.1.16',
+            appVersion: 'v10.1.17',
             versionPageUrl: 'https://github.com/YuHaiA/opaiRe/releases/latest',
             isLoggedIn: !!localStorage.getItem('auth_token'),
             loginPassword: '',
