28065d45fca7adee8eaa8adf60d7763d9cdf5493
YuHai~ <1501853627@qq.com~>
2026-04-18 20:29:23 +0800

    refactor: 拆分 v2rayN 子页并收敛代理页样式

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            appVersion: 'v10.1.8',
+            appVersion: 'v10.1.9',
             versionPageUrl: 'https://github.com/YuHaiA/opaiRe/releases/latest',
             isLoggedIn: !!localStorage.getItem('auth_token'),
             loginPassword: '',
