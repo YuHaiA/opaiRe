967f4dce9d7204a1fb6342a0cc860d07bf946fb2
YuHai~ <1501853627@qq.com~>
2026-04-24 23:20:16 +0800

    chore: bump version to v12.1.1

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            appVersion: 'v12.0.2',
+            appVersion: 'v12.1.1',
             versionPageUrl: 'https://github.com/YuHaiA/opaiRe/releases/latest',
             isLoggedIn: !!localStorage.getItem('auth_token'),
             loginPassword: '',
