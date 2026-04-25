a4e403c7cc5185812ffa5d5fda18f44b1c806dc5
YuHai~ <1501853627@qq.com~>
2026-04-26 01:22:21 +0800

    chore: set release version to 10.2.1

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            appVersion: 'v12.2.2',
+            appVersion: 'v10.2.1',
             versionPageUrl: 'https://github.com/YuHaiA/opaiRe/releases/latest',
             isLoggedIn: !!localStorage.getItem('auth_token'),
             loginPassword: '',
